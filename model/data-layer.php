f013834f2d938e360fd80e017556384127802b9e
iamjada <jsenebouttarath@mail.greenriver.edu>
2021-05-18 00:39:25 -0400

    Added data-layer.php

---
+++ model/data-layer.php
@@ -0,0 +1,11 @@
+<?php
+/* data-layer.php
+ * populated checkbox list using an array
+ *
+ */
+
+function getFlavors()
+{
+    return array("The Grasshopper", "Whiskey Maple Bacon", "Carrot Walnut",
+        "Salted Caramel Cupcake", "Red Velvet", "Lemon Drop", "Tiramisu");
+}
